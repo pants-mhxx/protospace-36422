28f9902c6c0160219e2b1fb02ba5c3ea3df29bc3
ShunKondo <stedra0917@gmail.com>
2021-08-02 11:06:39 +0900

    アクセス範囲制限

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'prototypes/index'
   root to: "prototypes#index"
+  
   resources :prototypes do
     resources :comments,  only: :create
     resources :users, only: :show
